39e83ba4525db8e5961eb2995d652ea2c92050ea
ameer eng <ameer20051975@gmail.com>
2025-02-19 21:13:27 +0300

    first

--- lib/utils.ts
+++ lib/utils.ts
@@ -16,3 +16,17 @@ export const toSlug = (text: string): string =>
     .replace(/[^\w\s-]+/g, '')
     .replace(/\s+/g, '-')
     .replace(/^-+|-+$/g, '')
+// PROMPT: [ChatGTP] create toTitleCase ts arrow function that capitalize the first letter of each word
+const CURRENCY_FORMATTER = new Intl.NumberFormat('en-US', {
+  currency: 'USD',
+  style: 'currency',
+  minimumFractionDigits: 2,
+})
+export function formatCurrency(amount: number) {
+  return CURRENCY_FORMATTER.format(amount)
+}
+
+const NUMBER_FORMATTER = new Intl.NumberFormat('en-US')
+export function formatNumber(number: number) {
+  return NUMBER_FORMATTER.format(number)
+}
